676a505d80e0c0f006fe0594866f2b0d577d3994
Nikolai-Cenkov <120922821+Nikolai-Cenkov@users.noreply.github.com>
2023-01-08 11:48:12 +0200

    Add files via upload

---
+++ SpeedInfo2_6.java
@@ -0,0 +1,22 @@
+package FirstStepsInCoding.Lab.ChecksLesson2;
+
+import java.util.Scanner;
+
+public class SpeedInfo2_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double speed = Double.parseDouble(scanner.nextLine());
+        if (speed <= 10) {
+            System.out.println("slow");
+        } else if (speed <= 50) {
+            System.out.println("average");
+        } else if (speed <= 150) {
+            System.out.println("fast");
+        } else if (speed <= 1000) {
+            System.out.println("ultra fast");
+        } else {
+            System.out.println("extremely fast");
+        }
+
+    }
+}
